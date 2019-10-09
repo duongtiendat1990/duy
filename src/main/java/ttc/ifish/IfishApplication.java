23438055d783b4f0c9eeedc85311881fb3dd8bac
Khương Duy Phan <duypk@ttc-solutions.com>
2019-10-09 10:12:20 +0700

    git init

---
+++ src/main/java/ttc/ifish/IfishApplication.java
@@ -0,0 +1,13 @@
+package ttc.ifish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfishApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(IfishApplication.class, args);
+  }
+
+}
